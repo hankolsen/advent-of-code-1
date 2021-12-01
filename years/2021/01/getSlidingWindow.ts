2a8a40a5cb3441a8c5e8f889c25b9c281a52a72b
Hank <hank@mullvad.net>
2021-12-01 19:53:38 +0100

    WIP part 2

--- years/2021/01/getSlidingWindow.ts
+++ years/2021/01/getSlidingWindow.ts
@@ -1,19 +1,17 @@
-type Prop = { previous: number[], count: number, previousSum: number };
-
 const getSlidingWindow = (input: number[]) => {
-  const { count } = input.reduce((acc: Prop, row) => {
-    acc.previous.push(row);
-    if (acc.previous.length === 3) {
-      const first = acc.previous.shift() ?? 0;
-      const [second, third] = acc.previous;
-      const sum = first + second + third;
-      if (sum > acc.previousSum) {
-        acc.previousSum = sum;
-        acc.count += 1;
-      }
+  return input.reduce(({ previous, count }, row, index, arr) => {
+    if (index >= arr.length - 2) {
+      return { previous, count };
     }
-    return acc;
-  }, { previous: [], count: -1, previousSum: -Infinity });
-  return count;
+    
+    const sum = row + arr[index + 1] + arr[index + 2];
+    if (index == 0) {
+      return { previous: sum, count: 0 };
+    }
+    if (sum > previous) {
+      return { previous: sum, count: count + 1 };
+    }
+    return { previous: sum, count };
+  }, { previous: 0, count: -1 })?.count;
 };
 export default getSlidingWindow;
