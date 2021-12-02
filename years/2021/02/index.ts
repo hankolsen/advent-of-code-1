3e54f7c40567533fe7768b1c91532122357ba358
hank <hank@hank.se>
2021-12-02 13:21:24 +0100

    Add Day 2

--- years/2021/02/index.ts
+++ years/2021/02/index.ts
@@ -1,9 +1,11 @@
-import _ from 'lodash';
+import _, { values } from 'lodash';
 import * as util from '../../../util/util';
 import * as test from '../../../util/test';
 import chalk from 'chalk';
 import { log, logSolution, trace } from '../../../util/log';
 import { performance } from 'perf_hooks';
+import { getRows } from '../../../util/input';
+import { dir } from 'console';
 
 const YEAR = 2021;
 const DAY = 2;
@@ -13,28 +15,87 @@ const DAY = 2;
 // problem url  : https://adventofcode.com/2021/day/2
 
 async function p2021day2_part1(input: string, ...params: any[]) {
-  return 'Not implemented';
+  const { x, y } = getRows(input).reduce(
+    ({ x, y }, row) => {
+      const [, direction, val] = row.match(/(forward|up|down) (\d+)/) ?? [];
+      if (direction === 'forward') {
+        x += Number(val);
+      }
+      if (direction === 'up') {
+        y -= Number(val);
+      }
+      if (direction === 'down') {
+        y += Number(val);
+      }
+      return { x, y };
+    },
+    { x: 0, y: 0 },
+  );
+
+  return x * y;
 }
 
 async function p2021day2_part2(input: string, ...params: any[]) {
-  return 'Not implemented';
+  const { x, y } = getRows(input).reduce(
+    ({ x, y, aim }, row) => {
+      const [, direction, val] = row.match(/(forward|up|down) (\d+)/) ?? [];
+      if (direction === 'forward') {
+        x += Number(val);
+        y += aim * Number(val);
+      }
+      if (direction === 'up') {
+        aim -= Number(val);
+      }
+      if (direction === 'down') {
+        aim += Number(val);
+      }
+      return { x, y, aim };
+    },
+    { x: 0, y: 0, aim: 0 },
+  );
+
+  return x * y;;
 }
 
 async function run() {
-  const part1tests: TestCase[] = [];
-  const part2tests: TestCase[] = [];
+  const part1tests: TestCase[] = [
+    {
+      input: `
+        forward 5
+        down 5
+        forward 8
+        up 3
+        down 8
+        forward 2
+			`,
+      expected: '150',
+    },
+  ];
+  const part2tests: TestCase[] = [
+    {
+      input: `
+        forward 5
+        down 5
+        forward 8
+        up 3
+        down 8
+        forward 2
+      `,
+      expected: '900',
+    },
+  ];
 
   // Run tests
   test.beginTests();
   await test.section(async () => {
     for (const testCase of part1tests) {
-	    test.logTestResult(testCase, String(await p2021day2_part1(testCase.input, ...(testCase.extraArgs || []))));
-	  }
+      test.logTestResult(testCase, String(await p2021day2_part1(testCase.input, ...(testCase.extraArgs || []))));
+    }
   });
   await test.section(async () => {
     for (const testCase of part2tests) {
-		  test.logTestResult(testCase, String(await p2021day2_part2(testCase.input, ...(testCase.extraArgs || []))));
-	  }
+      test.logTestResult(testCase, String(await p2021day2_part2(testCase.input, ...(testCase.extraArgs || []))));
+    }
   });
   test.endTests();
 
