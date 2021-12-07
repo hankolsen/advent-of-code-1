0007d88e3685950208614654800f03875787b19e
Hank <hank@mullvad.net>
2021-12-07 08:14:50 +0100

    Add Day 7 part 1

--- years/2021/07/index.ts
+++ years/2021/07/index.ts
@@ -1,8 +1,7 @@
-import _ from 'lodash';
 import * as util from '../../../util/util';
 import * as test from '../../../util/test';
 import chalk from 'chalk';
-import { log, logSolution, trace } from '../../../util/log';
+import { log, logSolution } from '../../../util/log';
 import { performance } from 'perf_hooks';
 
 const YEAR = 2021;
@@ -13,7 +12,15 @@ const DAY = 7;
 // problem url  : https://adventofcode.com/2021/day/7
 
 async function p2021day7_part1(input: string, ...params: any[]) {
-  return 'Not implemented';
+  const crabs = input.split(',').map(Number);
+  const min = Math.min(...crabs);
+  const max = Math.max(...crabs);
+  const positions = Array(max - min + 1).fill(0);
+  const fuelCosts = positions
+    .map((_, i) => crabs
+      .map((crab) => Math.abs(crab - i))
+      .reduce((a, b) => a + b, 0));
+  return Math.min(...fuelCosts);
 }
 
 async function p2021day7_part2(input: string, ...params: any[]) {
@@ -21,36 +28,46 @@ async function p2021day7_part2(input: string, ...params: any[]) {
 }
 
 async function run() {
-  const part1tests: TestCase[] = [];
-  const part2tests: TestCase[] = [];
-
+  const part1tests: TestCase[] = [
+    {
+      input: `16,1,2,0,4,2,7,1,2,14`,
+      expected: '37',
+    },
+  ];
+  const part2tests: TestCase[] = [
+    {
+      input: `16,1,2,0,4,2,7,1,2,14`,
+      expected: '168',
+    },
+  ];
+  
   // Run tests
   test.beginTests();
   await test.section(async () => {
     for (const testCase of part1tests) {
-	    test.logTestResult(testCase, String(await p2021day7_part1(testCase.input, ...(testCase.extraArgs || []))));
-	  }
+      test.logTestResult(testCase, String(await p2021day7_part1(testCase.input, ...(testCase.extraArgs || []))));
+    }
   });
   await test.section(async () => {
     for (const testCase of part2tests) {
-		  test.logTestResult(testCase, String(await p2021day7_part2(testCase.input, ...(testCase.extraArgs || []))));
-	  }
+      test.logTestResult(testCase, String(await p2021day7_part2(testCase.input, ...(testCase.extraArgs || []))));
+    }
   });
   test.endTests();
-
+  
   // Get input and run program while measuring performance
   const input = await util.getInput(DAY, YEAR);
-
+  
   const part1Before = performance.now();
   const part1Solution = String(await p2021day7_part1(input));
   const part1After = performance.now();
-
+  
   const part2Before = performance.now();
   const part2Solution = String(await p2021day7_part2(input));
   const part2After = performance.now();
-
+  
   logSolution(7, 2021, part1Solution, part2Solution);
-
+  
   log(chalk.gray('--- Performance ---'));
   log(chalk.gray(`Part 1: ${util.formatTime(part1After - part1Before)}`));
   log(chalk.gray(`Part 2: ${util.formatTime(part2After - part2Before)}`));
