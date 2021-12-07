100778d525cbde84b1d8b71fac2ed77b04fc3049
Hank <hank@mullvad.net>
2021-12-07 21:19:40 +0100

    Add Day 4 part 2

--- years/2021/04/index.ts
+++ years/2021/04/index.ts
@@ -14,65 +14,60 @@ const DAY = 4;
 
 const parseBoards = (rows: string[]) => {
   const boards = [];
-  while (rows.length > 5) {
-    rows.splice(0, 1);
-    const board = rows.splice(0, 5).map((row) => {
-      const [, ...numbers] = row.match(/(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)/) || [];
-      return numbers.map(Number);
-    });
-    let boardColumns: number[][] = [[]];
-    for (let x = 0; x < 5; x += 1) {
-      boardColumns[x] = [];
-      for (let y = 0; y < 5; y += 1) {
-        boardColumns[x].push(board[y][x]);
-      }
-    }
-    boards.push({ board, boardColumns });
+  while (rows.length > 4) {
+    const board = rows.splice(0, 5).map((row) => row.split(/\s+/).filter((c) => c !== '').map(Number));
+    boards.push(board);
   }
   return boards;
 };
 
-async function p2021day4_part1(input: string, ...params: any[]) {
-  const rows = getRows(input);
-  const winningNumbers = rows.splice(0, 1)[0].split(',').map(Number);
+const horizontalWin = (board: number[][], numbers: number[]) => board.some((row) => row.every((number) => numbers.includes(number)));
+const verticalWin = (board: number[][], numbers: number[]) => board.map((_, col) => board.map((row) => row[col])).some((c) => c.every((number) => numbers.includes(number)));
+const checkBoard = (board: number[][], numbers: number[]) => horizontalWin(board, numbers) || verticalWin(board, numbers);
+
+const play = (input: string, isPart2: boolean = false) => {
+  const rows = getRows(input).filter((row) => row !== '');
+  const winningNumbers = rows.shift()!.split(',').map(Number);
   const boards = parseBoards(rows);
-  let bingo = false;
-  let count = 5;
-  let winningBoardNumber = -1;
-  let numbers: number[] = [];
-  while (!bingo) {
-    numbers = winningNumbers.slice(0, count);
-    boards.forEach(({ board, boardColumns }, i) => {
-      board.forEach((row) => {
-        if (row.every((number) => numbers.includes(number))) {
-          bingo = true;
-          winningBoardNumber = i;
+  let winningNumber = -1;
+  let winningBoard: number[][] = [];
+  let drawnNumbers: number[];
+  winningNumbers.some((number, index) => {
+    drawnNumbers = winningNumbers.slice(0, index + 1);
+    return boards.some((board, boardNumber) => {
+      if (checkBoard(board, drawnNumbers)) {
+        if (isPart2) {
+          if (boards.length === 1) {
+            winningNumber = number;
+            winningBoard = board;
+            return true;
+          }
+          boards.splice(boardNumber, 1);
+        } else {
+          winningNumber = number;
+          winningBoard = board;
+          return true;
         }
-      });
-      boardColumns.forEach((column) => {
-        if (column.every((number) => numbers.includes(number))) {
-          bingo = true;
-          winningBoardNumber = i;
-        }
-      });
-    });
-    count += 1;
-  }
-  const winningBoard = boards[winningBoardNumber];
-  let unMarkedNumbers = 0;
-  const winner = numbers[numbers.length - 1];
-  winningBoard.board.forEach((row) => {
-    row.forEach((cell) => {
-      if (!numbers.includes(cell)) {
-        unMarkedNumbers += cell;
       }
     });
   });
-  return unMarkedNumbers * winner
+  const unMarkedNumbers = winningBoard.reduce((acc, row) => {
+    return acc + row.reduce((sum, cell) => {
+      if (!drawnNumbers.includes(cell)) {
+        sum += cell;
+      }
+      return sum;
+    }, 0);
+  }, 0);
+  return unMarkedNumbers * winningNumber;
+};
+
+async function p2021day4_part1(input: string, ...params: any[]) {
+  return play(input);
 }
 
 async function p2021day4_part2(input: string, ...params: any[]) {
-  return 'Not implemented';
+  return play(input, true);
 }
 
 async function run() {
@@ -101,7 +96,29 @@ async function run() {
       expected: '4512',
     },
   ];
-  const part2tests: TestCase[] = [];
+  const part2tests: TestCase[] = [{
+    input: `
+7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7`,
+    expected: '1924',
+  }];
   
   // Run tests
   test.beginTests();
