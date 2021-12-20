0566805214557981bd3c7d86dc34e21c58aae8f8
hank <hank@hank.se>
2021-12-20 20:27:11 +0100

    Add Day 14 part 1

--- years/2021/14/index.ts
+++ years/2021/14/index.ts
@@ -1,9 +1,9 @@
-import _ from 'lodash';
 import * as util from '../../../util/util';
 import * as test from '../../../util/test';
 import chalk from 'chalk';
-import { log, logSolution, trace } from '../../../util/log';
+import { log, logSolution } from '../../../util/log';
 import { performance } from 'perf_hooks';
+import { getRows } from '../../../util/input';
 
 const YEAR = 2021;
 const DAY = 14;
@@ -13,7 +13,32 @@ const DAY = 14;
 // problem url  : https://adventofcode.com/2021/day/14
 
 async function p2021day14_part1(input: string, ...params: any[]) {
-  return 'Not implemented';
+  const [first, , ...rest] = getRows(input);
+  let template = first.split('');
+  const rules = rest.reduce((acc: { [key: string]: string }, entry) => {
+    const [rule, result] = entry.split(' -> ');
+    acc[rule] = result;
+    return acc;
+  }, {});
+  for (let i = 0; i < 10; i += 1) {
+    let index = 0;
+    while (index < template.length - 1) {
+      const char = template[index];
+      const pair = `${char}${template[index + 1]}`;
+      const res = rules[pair];
+      template = [...template.slice(0, index + 1), res, ...template.slice(index + 1)];
+      index += 2;
+    }
+  }
+
+  const counts = template.reduce((acc: { [key: string]: number }, e) => {
+    acc[e] = acc[e] ? acc[e] + 1 : 1;
+    return acc;
+  }, {});
+
+  const max = Math.max(...Object.values(counts));
+  const min = Math.min(...Object.values(counts));
+  return max - min;
 }
 
 async function p2021day14_part2(input: string, ...params: any[]) {
@@ -21,20 +46,44 @@ async function p2021day14_part2(input: string, ...params: any[]) {
 }
 
 async function run() {
-  const part1tests: TestCase[] = [];
+  const part1tests: TestCase[] = [
+    {
+      input: `
+NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C
+`,
+      expected: '1588',
+    },
+  ];
   const part2tests: TestCase[] = [];
 
   // Run tests
   test.beginTests();
   await test.section(async () => {
     for (const testCase of part1tests) {
-	    test.logTestResult(testCase, String(await p2021day14_part1(testCase.input, ...(testCase.extraArgs || []))));
-	  }
+      test.logTestResult(testCase, String(await p2021day14_part1(testCase.input, ...(testCase.extraArgs || []))));
+    }
   });
   await test.section(async () => {
     for (const testCase of part2tests) {
-		  test.logTestResult(testCase, String(await p2021day14_part2(testCase.input, ...(testCase.extraArgs || []))));
-	  }
+      test.logTestResult(testCase, String(await p2021day14_part2(testCase.input, ...(testCase.extraArgs || []))));
+    }
   });
   test.endTests();
 
