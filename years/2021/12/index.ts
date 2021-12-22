7f8e59b783f01c8f0572de3f47f1004d8902b948
hank <hank@hank.se>
2021-12-22 10:16:56 +0100

    Add Day 12 part 1

--- years/2021/12/index.ts
+++ years/2021/12/index.ts
@@ -4,6 +4,7 @@ import * as test from '../../../util/test';
 import chalk from 'chalk';
 import { log, logSolution, trace } from '../../../util/log';
 import { performance } from 'perf_hooks';
+import { getRows } from '../../../util/input';
 
 const YEAR = 2021;
 const DAY = 12;
@@ -12,8 +13,36 @@ const DAY = 12;
 // data path    : /Users/hank/projects/aoc/advent-of-code-1/years/2021/12/data.txt
 // problem url  : https://adventofcode.com/2021/day/12
 
+
+const parseInput = (input: string) => {
+  const paths = new Map<string, string[]>();
+  getRows(input).map((row) => {
+    const [start, stop] = row.split('-');
+    paths.set(start, [...(paths.get(start) ?? []), stop]);
+    paths.set(stop, [...(paths.get(stop) ?? []), start]);
+  });
+  return paths;
+}
+
 async function p2021day12_part1(input: string, ...params: any[]) {
-  return 'Not implemented';
+  const paths = parseInput(input);
+  const foundPaths = [];
+  const findPath = (pos: string, current: string[]) => {
+    if (pos === 'end') {
+      foundPaths.push([...current, pos]);
+      return;
+    }
+    const next = [...current, pos];
+    // @ts-ignore
+    for (const direction of paths.get(pos)) {
+      if (direction.toLowerCase() !== direction || !current.includes(direction)) {
+        findPath(direction, next);
+      }
+    }
+  };
+
+  findPath('start', []);
+  return foundPaths.length;
 }
 
 async function p2021day12_part2(input: string, ...params: any[]) {
@@ -21,7 +50,58 @@ async function p2021day12_part2(input: string, ...params: any[]) {
 }
 
 async function run() {
-  const part1tests: TestCase[] = [];
+  const part1tests: TestCase[] = [
+    {
+      input: `
+start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end
+`,
+      expected: '10'
+    },
+    {
+      input: `
+dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc
+`,
+      expected: '19'
+    },
+    {
+      input: `
+fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW
+`,
+      expected: '226'
+    }
+  ];
   const part2tests: TestCase[] = [];
 
   // Run tests
