c4485247aad8465f7eec98840004896d49fea2ff
Hank <hank@mullvad.net>
2022-12-03 15:33:03 +0100

    Refactor 2022 Day 3

--- years/2022/03/findGroupDuplicateValues.ts
+++ years/2022/03/findGroupDuplicateValues.ts
@@ -6,18 +6,18 @@ export const findGroupDuplicateValues = (input: string, groupSize: number = 3) =
   const rucksacks: Rucksack[] = getRowsAsArrays(input);
 
   let group: RucksackGroup = [];
-  
-	return rucksacks.reduce((acc, rucksack, i) => {
+
+  return rucksacks.reduce((acc, rucksack, i) => {
     group.push(rucksack);
-    
-		if ((i + 1) % groupSize === 0) {
-			const [firstRucksack, ...otherRucksacks] = group;
-			firstRucksack.some((item) => {
-				if (otherRucksacks.every((rucksack) => rucksack.includes(item))) {
-					acc += getItemValue(item);
-					return true;
-				}
-			});
+
+    if ((i + 1) % groupSize === 0) {
+      const [firstRucksack, ...otherRucksacks] = group;
+      firstRucksack.some((item) => {
+        if (otherRucksacks.every((rucksack) => rucksack.includes(item))) {
+          acc += getItemValue(item);
+          return true;
+        }
+      });
       group = [];
     }
     return acc;
