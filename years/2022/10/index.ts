5e09b688c506d7eaeea9d37627e974813b6ec71d
Hank <hank@mullvad.net>
2022-12-10 20:41:10 +0100

    Add day 10 part 1

---
+++ years/2022/10/index.ts
@@ -0,0 +1,241 @@
+import _ from 'lodash';
+import * as util from '../../../util/util';
+import * as test from '../../../util/test';
+import chalk from 'chalk';
+import { log, logSolution, trace } from '../../../util/log';
+import { performance } from 'perf_hooks';
+import { getRows } from '../../../util/input';
+
+const YEAR = 2022;
+const DAY = 10;
+
+// solution path: /Users/hank/projects/aoc/advent-of-code-1/years/2022/10/index.ts
+// data path    : /Users/hank/projects/aoc/advent-of-code-1/years/2022/10/data.txt
+// problem url  : https://adventofcode.com/2022/day/10
+
+async function p2022day10_part1(input: string, ...params: any[]) {
+  const rows = getRows(input);
+  let cycle = 0;
+  let x = 1;
+  let values: Record<number, number> = {};
+  
+  const tick = () => {
+    cycle += 1;
+    if (cycle === 20 || ((cycle - 20) % 40 === 0)) {
+      values[cycle] = x;
+    }
+  };
+  
+  rows.forEach((row) => {
+    const [instruction, value] = row.split(' ');
+    tick();
+    if (instruction === 'addx') {
+      tick();
+      x += Number(value);
+    }
+  });
+  
+  return Object.entries(values).reduce((prod, [key, value]) => {
+    prod += Number(key) * value;
+    return prod;
+  }, 0);
+}
+
+async function p2022day10_part2(input: string, ...params: any[]) {
+  return 'Not implemented';
+}
+
+async function run() {
+  const part1tests: TestCase[] = [
+    {
+      input: `addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`,
+      expected: '13140',
+    },
+  ];
+  const part2tests: TestCase[] = [];
+  
+  // Run tests
+  test.beginTests();
+  await test.section(async () => {
+    for (const testCase of part1tests) {
+      test.logTestResult(testCase, String(await p2022day10_part1(testCase.input, ...(testCase.extraArgs || []))));
+    }
+  });
+  await test.section(async () => {
+    for (const testCase of part2tests) {
+      test.logTestResult(testCase, String(await p2022day10_part2(testCase.input, ...(testCase.extraArgs || []))));
+    }
+  });
+  test.endTests();
+  
+  // Get input and run program while measuring performance
+  const input = await util.getInput(DAY, YEAR);
+  
+  const part1Before = performance.now();
+  const part1Solution = String(await p2022day10_part1(input));
+  const part1After = performance.now();
+  
+  const part2Before = performance.now();
+  const part2Solution = String(await p2022day10_part2(input));
+  const part2After = performance.now();
+  
+  logSolution(10, 2022, part1Solution, part2Solution);
+  
+  log(chalk.gray('--- Performance ---'));
+  log(chalk.gray(`Part 1: ${util.formatTime(part1After - part1Before)}`));
+  log(chalk.gray(`Part 2: ${util.formatTime(part2After - part2Before)}`));
+  log();
+}
+
+run()
+  .then(() => {
+    process.exit();
+  })
+  .catch((error) => {
+    throw error;
+  });
