88118fd8e52ae9bc71d2d267140f5e17ee4e4685
Hank <hank@mullvad.net>
2022-12-10 20:54:36 +0100

    Add day 10 part 2

--- years/2022/10/index.ts
+++ years/2022/10/index.ts
@@ -42,7 +42,37 @@ async function p2022day10_part1(input: string, ...params: any[]) {
 }
 
 async function p2022day10_part2(input: string, ...params: any[]) {
-  return 'Not implemented';
+  const rows = getRows(input);
+  let cycle = 0;
+  let x = 1;
+  let pixels: string[] = [];
+  
+  const pixelIsTouchingSprite = () => {
+    const pos = (cycle - 1) % 40;
+    return x === pos || x - 1 === pos || x + 1 === pos;
+  };
+  const tick = () => {
+    cycle += 1;
+    pixels[cycle] = pixelIsTouchingSprite() ? '#' : '.';
+  };
+  
+  rows.forEach((row) => {
+    const [instruction, value] = row.split(' ');
+    tick();
+    if (instruction === 'addx') {
+      tick();
+      x += Number(value);
+    }
+  });
+  
+  let row: string[] = [];
+  pixels.forEach((pixel, i) => {
+    row.push(pixel);
+    if (i % 40 === 0) {
+      console.log(row.join(''));
+      row = [];
+    }
+  })
 }
 
 async function run() {
@@ -197,7 +227,157 @@ noop`,
       expected: '13140',
     },
   ];
-  const part2tests: TestCase[] = [];
+  const part2tests: TestCase[] = [
+    {
+      input: `addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`,
+      expected: '13140',
+    },
+  ];
   
   // Run tests
   test.beginTests();
