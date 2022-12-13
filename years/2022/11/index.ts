a0b794663180267ad00e6473c1a380dfbfb1943e
Hank <hank@mullvad.net>
2022-12-13 18:32:29 +0100

    Add day 11 part 2

--- years/2022/11/index.ts
+++ years/2022/11/index.ts
@@ -14,6 +14,8 @@ const DAY = 11;
 
 const parseInput = (input: string) => {
   const monkeys: Record<number, { items: number[], operation: string, test: number, trueTo: number, falseTo: number }> = {};
+  let supermod = 1;
+  
   input.split('\n\n').forEach((rows) => {
     const [, monkey] = rows.match(/Monkey (\d+)/) ?? [];
     const [, numbers] = rows.match(/Starting items: (.*)\n/) ?? [];
@@ -29,12 +31,13 @@ const parseInput = (input: string) => {
       trueTo: Number(trueTo),
       falseTo: Number(falseTo),
     };
+    supermod *= Number(test);
   });
-  return monkeys;
+  return { monkeys, supermod };
 };
 
 async function p2022day11_part1(input: string, ...params: any[]) {
-  const monkeys = parseInput(input);
+  const { monkeys } = parseInput(input);
   const inspections: Record<number, number> = {};
   
   for (let round = 1; round <= 20; round += 1) {
@@ -62,7 +65,32 @@ async function p2022day11_part1(input: string, ...params: any[]) {
 }
 
 async function p2022day11_part2(input: string, ...params: any[]) {
-  return 'Not implemented';
+  const { monkeys, supermod } = parseInput(input);
+  const inspections: Record<number, number> = {};
+  
+  for (let round = 1; round <= 10000; round += 1) {
+    let monkey = 0;
+    while (monkey < Object.entries(monkeys).length) {
+      const { items, operation, test, trueTo, falseTo } = monkeys[monkey];
+      while (items.length) {
+        inspections[monkey] = inspections[monkey] ? inspections[monkey] += 1 : 1;
+        let item = items.shift();
+        item = item! % supermod;
+        let worryLevel = eval(operation.replace(/old/g, item.toString()));
+        if (worryLevel % test === 0) {
+          monkeys[trueTo].items.push(worryLevel);
+        } else {
+          monkeys[falseTo].items.push(worryLevel);
+        }
+      }
+      monkey += 1;
+    }
+  }
+  const sorted = Object.entries(inspections).sort(([monkeyA, countA], [monkeyB, countB]) => countB - countA);
+  return (sorted.slice(0, 2).reduce((prod, [, count]) => {
+    prod *= count;
+    return prod;
+  }, 1));
 }
 
 async function run() {
@@ -98,7 +126,38 @@ Monkey 3:
       expected: '10605',
     },
   ];
-  const part2tests: TestCase[] = [];
+  const part2tests: TestCase[] = [
+    {
+      input: `Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1`,
+      expected: '2713310158',
+    },
+  ];
   
   // Run tests
   test.beginTests();
